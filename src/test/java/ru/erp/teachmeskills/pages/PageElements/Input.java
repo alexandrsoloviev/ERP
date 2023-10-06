6166e864d1ee609ed44be42edaa1d7589522a86c
asalauyou <a.salauyou@arateg.com>
2023-10-06 16:04:40 +0300

    fix selectors

--- src/test/java/ru/erp/teachmeskills/pages/PageElements/Input.java
+++ src/test/java/ru/erp/teachmeskills/pages/PageElements/Input.java
@@ -5,10 +5,10 @@ import static com.codeborne.selenide.Selenide.$;
 public class Input {
 
     public void setValueInInput(String placeholder,String value){
-        $("[placeholder="+placeholder+"]").setValue(value);
+        $("[placeholder=\""+placeholder+"\"]").setValue(value);
     }
 
     public void clearInput(String placeholder){
-        $("[placeholder="+placeholder+"]").clear();
+        $("[placeholder=\""+placeholder+"\"]").clear();
     }
 }
