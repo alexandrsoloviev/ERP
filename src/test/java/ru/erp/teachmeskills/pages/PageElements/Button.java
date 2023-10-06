3e39b689c7b397e44fb019a8e632d931387a5c44
asalauyou <a.salauyou@arateg.com>
2023-10-06 16:11:57 +0300

    fix selectors in Button

--- src/test/java/ru/erp/teachmeskills/pages/PageElements/Button.java
+++ src/test/java/ru/erp/teachmeskills/pages/PageElements/Button.java
@@ -1,11 +1,12 @@
 package ru.erp.teachmeskills.pages.PageElements;
 
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class Button {
 
     public void clickButton(String buttonText){
-        $("button").$(withText(buttonText)).click();
+        $(byTagAndText("button",buttonText)).click();
     }
 }
