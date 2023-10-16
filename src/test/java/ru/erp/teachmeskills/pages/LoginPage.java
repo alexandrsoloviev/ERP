16e8ea4542024ba664d1e39e2fe06c2b376187a5
asalauyou <a.salauyou@arateg.com>
2023-10-16 13:39:14 +0300

    add remote.properties

--- src/test/java/ru/erp/teachmeskills/pages/LoginPage.java
+++ src/test/java/ru/erp/teachmeskills/pages/LoginPage.java
@@ -14,11 +14,11 @@ public class LoginPage extends BasePage {
     private final SelenideElement authForm = $("form");
 
     public void openLoginPage() {
-        open("/loginz");
+        open("/login");
     }
 
     public void setEmailValue(String email) {
-        input.setValueInInput("Введите Email",email);
+        input.setValueInInput("Ввеasasдите Email",email);
     }
 
     public void setPasswordValue(String pass) {
